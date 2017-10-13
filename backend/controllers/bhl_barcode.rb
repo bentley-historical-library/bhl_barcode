6b553dc729ffc845c6ce6685584df10c387c995c
djpillen@umich.edu <djpillen@umich.edu>
2017-10-13 14:51:02 -0400

    uncommitted changes

--- backend/controllers/bhl_barcode.rb
+++ backend/controllers/bhl_barcode.rb
@@ -1,22 +1,22 @@
 class ArchivesSpaceService < Sinatra::Base
 
   Endpoint.get('/repositories/:repo_id/find_by_barcode/container')
-    .description("Get a Top Container URI for a given barcode")
+    .description("Get a Top Container for a given barcode")
     .params(["repo_id", :repo_id],
-    		    ["barcode", String])
+            ["barcode", String])
     .permissions([:view_repository])
-    .returns([200, "Top Container"]) \
+    .returns([200, "TopContainer JSON"]) \
   do
     json_response(BHLBarcode.find_by_barcode(TopContainer, {:repo_id => params[:repo_id], :barcode => params[:barcode]}))
   end
 
   Endpoint.get('/repositories/:repo_id/find_by_barcode/location')
-    .description("Get a Location URI for a given barcode")
+    .description("Get a Location for a given barcode")
     .params(["repo_id", :repo_id],
-    		    ["barcode", String]
+            ["barcode", String]
             )
     .permissions([:view_repository])
-    .returns([200, "Location"]) \
+    .returns([200, "Location JSON"]) \
   do
     json_response(BHLBarcode.find_by_barcode(Location, {:barcode => params[:barcode]}))
   end
